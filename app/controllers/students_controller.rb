a15beaa241e0c9670165e4943782611546499217
Brian Diaz <bidiaz101@gmail.com>
2021-11-18 13:02:20 -0600

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,19 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+
+    if(params[:name]) 
+      students = students.filter do |student|
+        student.to_s.downcase.include?(params[:name].downcase)
+      end
+    end
+
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
